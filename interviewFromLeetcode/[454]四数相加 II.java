f6cc165fd85775c4d2d7052edcf6e04651f74973
Zgxh <1227814546@qq.com>
2020-04-17 13:26:47 +0800

    2020-4-17 mid难度，hashMap的简单应用

---
+++ interviewFromLeetcode/[454]四数相加 II.java
@@ -0,0 +1,64 @@
+//给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[
+//l] = 0。 
+//
+// 为了使问题简单化，所有的 A, B, C, D 具有相同的长度 N，且 0 ≤ N ≤ 500 。所有整数的范围在 -228 到 228 - 1 之间，最
+//终结果不会超过 231 - 1 。 
+//
+// 例如: 
+//
+// 
+//输入:
+//A = [ 1, 2]
+//B = [-2,-1]
+//C = [-1, 2]
+//D = [ 0, 2]
+//
+//输出:
+//2
+//
+//解释:
+//两个元组如下:
+//1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+//2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+// 
+// Related Topics 哈希表 二分查找
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    /**
+     * 四个数组分成2组，其中一组组内求出加和，用map存起来。
+     * 对另一组的所有组合进行遍历，满足条件的结果加起来。
+     * @param A
+     * @param B
+     * @param C
+     * @param D
+     * @return
+     */
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+
+        Map<Integer, Integer> map = new HashMap<>(); // sum -> times
+        int result = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                int sum = A[i] + B[j];
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+
+        for (int i = 0; i < C.length; i++) {
+            for (int j = 0; j < D.length; j++) {
+                int sum = C[i] + D[j];
+                result += map.getOrDefault(-sum, 0);
+            }
+        }
+
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
