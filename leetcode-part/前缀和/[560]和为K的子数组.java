2801fe122c1708a128b07b7fd0d4404cbe1393c2
YuYang <1227814546@qq.com>
2020-06-30 13:54:30 +0800

    2020-6-30 [mid] 前缀和

---
+++ leetcode-part/前缀和/[560]和为K的子数组.java
@@ -0,0 +1,47 @@
+//给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。 
+//
+// 示例 1 : 
+//
+// 
+//输入:nums = [1,1,1], k = 2
+//输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
+// 
+//
+// 说明 : 
+//
+// 
+// 数组的长度为 [1, 20,000]。 
+// 数组中元素的范围是 [-1000, 1000] ，且整数 k 的范围是 [-1e7, 1e7]。 
+// 
+// Related Topics 数组 哈希表
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    /**
+     * 思路：前缀和，记录从开头到每个位置的连续数组的sum与出现的次数。
+     * 并使用哈希表来加速查找。
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int count = 0, sum = 0;
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) { // 两个前缀和相减为k，则两个右边界之间的数组即为所求
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
