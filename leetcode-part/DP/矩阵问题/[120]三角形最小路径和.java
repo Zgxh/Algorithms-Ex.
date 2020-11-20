da12d177be7288c55ed7e87cfd172db91ab988f3
YuYang <1227814546@qq.com>
2020-11-20 16:00:02 +0800

    2020-11-20 [mid] DP for matrix problems

---
+++ leetcode-part/DP/矩阵问题/[120]三角形最小路径和.java
@@ -0,0 +1,53 @@
+//给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。 
+//
+// 相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。 
+//
+// 
+//
+// 例如，给定三角形： 
+//
+// [
+//     [2],
+//    [3,4],
+//   [6,5,7],
+//  [4,1,8,3]
+//]
+// 
+//
+// 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。 
+//
+// 
+//
+// 说明： 
+//
+// 如果你可以只使用 O(n) 的额外空间（n 为三角形的总行数）来解决这个问题，那么你的算法会很加分。 
+// Related Topics 数组 动态规划 
+// 👍 645 👎 0
+
+
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    // 简单的DP。
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] f = new int[n]; // 直接开辟最大容量作为 DP 数组
+        f[0] = triangle.get(0).get(0);
+        // 每一层都会多一个数，因此倒着来：从每一层的后至前来更新 f 数组
+        for (int i = 1; i < n; ++i) {
+            f[i] = f[i - 1] + triangle.get(i).get(i);
+            for (int j = i - 1; j > 0; --j) {
+                f[j] = Math.min(f[j - 1], f[j]) + triangle.get(i).get(j);
+            }
+            f[0] += triangle.get(i).get(0);
+        }
+        int minTotal = f[0];
+        for (int i = 1; i < n; ++i) {
+            minTotal = Math.min(minTotal, f[i]);
+        }
+        return minTotal;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
--- leetcode-part/DP/矩阵问题/[174]地下城游戏.java
+++ leetcode-part/DP/矩阵问题/[174]地下城游戏.java
@@ -52,26 +52,24 @@ import java.util.Arrays;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 
-    /**
-     * 思路：简单的DP问题。
-     * 从P开始，往回走，最低生命为1，依次计算从当前格子走到P点所需要的最少的初始生命。走到P点后生命值最少剩1.
-     * @param dungeon
-     * @return
-     */
+    // 此题的重点在于不光要让到达终点后的生命值大于0，在路上也必须大于0
     public int calculateMinimumHP(int[][] dungeon) {
         int m = dungeon.length, n = dungeon[0].length;
-        int[][] dp = new int[m + 1][n + 1];
+        // dp[i][j]: 要想从[i][j]到达终点，在走到[i][j]位置前，至少需要多少初始生命(不包含[i][j]提供的)
+        int[][] dp = new int[m + 1][n + 1]; // dp[0~m-1][0~n-1] 对应 dungeon[0~m-1][0~n-1]
         for (int i = 0; i <= m; i++) {
             Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
+        // 多加的一行一列是为了保证到达终点后，剩余生命值至少为 1。因为 dp 维护的是到达该点之前的最小生命值。
         dp[m][n - 1] = 1;
         dp[m - 1][n] = 1;
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                int minLife = Math.min(dp[i + 1][j], dp[i][j + 1]);
-                dp[i][j] = Math.max(1, minLife - dungeon[i][j]); // 注意最低生命是1
+                int minLife = Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j]; // 至少多少生命，就是最小的初始生命，所以 min
+                dp[i][j] = Math.max(1, minLife); // 到达每个点之前的初始生命必须大于0
             }
         }
+
         return dp[0][0];
     }
 }
